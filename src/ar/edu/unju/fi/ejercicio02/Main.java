879fd599040a87ade33b4a1c720f900eaea090ae
Usuario <Usuario@DESKTOP-UAQ2Q4U>
2024-04-04 15:58:09 -0300

    Ejercicio 2- Jarma Elias

---
+++ src/ar/edu/unju/fi/ejercicio02/Main.java
@@ -0,0 +1,14 @@
+package ar.edu.unju.fi.ejercicio02;
+
+public class Main {
+	
+	public static void main (String[] args) { 
+		String pais = "Argentina";
+		int edad = 18;
+		double altura = 15.5 , precio = 20.99; 
+		long telefono = 3881532567l;
+		double resultado = Math.cos(0.5); 
+		
+		System.out.println(pais+", "+edad+", "+altura+", "+precio+ ","+ telefono + ", " + resultado);
+	}
+}
